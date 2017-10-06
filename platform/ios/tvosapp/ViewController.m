0330d9297b4ed642c9a306547ec13ae0ae486fd3
Minh Nguyễn <mxn@1ec5.org>
2017-10-05 23:31:57 -0700

    [tvos] Press Play to tour world

--- platform/ios/tvosapp/ViewController.m
+++ platform/ios/tvosapp/ViewController.m
@@ -2,13 +2,22 @@
 
 #import <Mapbox/Mapbox.h>
 
+static const CLLocationCoordinate2D WorldTourDestinations[] = {
+    { .latitude = 38.9099711, .longitude = -77.0361123 },
+    { .latitude = 37.7884307, .longitude = -122.3998631 },
+    { .latitude = 12.9813016, .longitude = 77.6405126 },
+    { .latitude = -13.155846, .longitude = -74.2178934 },
+};
+
 @interface ViewController () <MGLMapViewDelegate>
 
 @property (weak, nonatomic) IBOutlet MGLMapView *mapView;
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    BOOL _isTouringWorld;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -21,5 +30,40 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)startWorldTour:(id)sender {
+    _isTouringWorld = YES;
+
+    [self.mapView removeAnnotations:self.mapView.annotations];
+    NSUInteger numberOfAnnotations = sizeof(WorldTourDestinations) / sizeof(WorldTourDestinations[0]);
+    NSMutableArray *annotations = [NSMutableArray arrayWithCapacity:numberOfAnnotations];
+    for (NSUInteger i = 0; i < numberOfAnnotations; i++) {
+        MGLPointAnnotation *annotation = [[MGLPointAnnotation alloc] init];
+        annotation.coordinate = WorldTourDestinations[i];
+        [annotations addObject:annotation];
+    }
+    [self.mapView addAnnotations:annotations];
+    [self continueWorldTourWithRemainingAnnotations:annotations];
+}
+
+- (void)continueWorldTourWithRemainingAnnotations:(NSMutableArray<MGLPointAnnotation *> *)annotations {
+    MGLPointAnnotation *nextAnnotation = annotations.firstObject;
+    if (!nextAnnotation || !_isTouringWorld) {
+        _isTouringWorld = NO;
+        return;
+    }
+    
+    [annotations removeObjectAtIndex:0];
+    MGLMapCamera *camera = [MGLMapCamera cameraLookingAtCenterCoordinate:nextAnnotation.coordinate
+                                                            fromDistance:10
+                                                                   pitch:arc4random_uniform(60)
+                                                                 heading:arc4random_uniform(360)];
+    __weak ViewController *weakSelf = self;
+    [self.mapView flyToCamera:camera completionHandler:^{
+        ViewController *strongSelf = weakSelf;
+        [strongSelf performSelector:@selector(continueWorldTourWithRemainingAnnotations:)
+                         withObject:annotations
+                         afterDelay:2];
+    }];
+}
 
 @end
