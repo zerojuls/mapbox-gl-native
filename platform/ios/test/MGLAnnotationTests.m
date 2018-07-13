dcd0d75d64242d9b03f056fa3310be63b7e7c428
jmkiley <jordan.kiley@mapbox.com>
2018-07-13 11:21:01 -0700

    [ios] testing out tests

--- platform/ios/test/MGLAnnotationTests.m
+++ platform/ios/test/MGLAnnotationTests.m
@@ -14,11 +14,34 @@
     [super setUp];
     _mapView = [[MGLMapView alloc] initWithFrame:CGRectMake(0, 0, 64, 64)];
     _mapView.delegate = self;
-    _mapView.accessibilityIdentifier = @"Map View";
+    _mapView.accessibilityLabel = @"Map View";
 }
 
 - (void)testTappingDisabledPolyline {
-    XCUIApplication *app = [[XCUIApplication alloc] init];
-    XCUIElement *element = 
     
+    CLLocationCoordinate2D coordinates[] = {
+        _mapView.visibleCoordinateBounds.ne,
+        _mapView.centerCoordinate,
+        _mapView.visibleCoordinateBounds.sw
+    };
+    
+    MGLPolyline *polyline = [MGLPolyline polylineWithCoordinates:coordinates count:3];
+    polyline.enabled = NO;
+    polyline.accessibilityLabel = @"Polyline";
+    [_mapView addAnnotation:polyline];
+    
+    // Code to simulate tap
+    // Getting 
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElement *mapElement = [[app descendantsMatchingType:XCUIElementTypeAny] elementMatchingPredicate:[NSPredicate predicateWithFormat:@"accessibilityLabel == 'Map View'"]];
+//    XCUIElement *lineElement = [[app descendantsMatchingType:XCUIElementTypeAny] elementMatchingPredicate:[NSPredicate predicateWithFormat:@"accessibilityLabel == 'Polyline'"]];
+    
+    CGPoint point = [_mapView convertCoordinate:_mapView.centerCoordinate toPointToView:_mapView];
+    
+    XCUICoordinate *center = [mapElement coordinateWithNormalizedOffset:CGVectorMake(point.x, point.y)];
+    [center tap];
+    
+    XCTAssertNil(_mapView.selectedAnnotations.firstObject, @"There should be no selected annotation");
 }
+
+@end
